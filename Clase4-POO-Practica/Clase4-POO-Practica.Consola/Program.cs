4835f6cc3f606c0f912ed941cfa6fa4a427b76f4
Pablo Sanchez <psanchez@plus54.com>
2021-04-28 21:10:30 -0300

    agregando entidades y proyecto de consola

---
+++ Clase4-POO-Practica/Clase4-POO-Practica.Consola/Program.cs
@@ -0,0 +1,15 @@
+﻿using Entidades;
+using System;
+
+namespace Clase4_POO_Practica.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+          
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Clase4-POO-Practica/Entidades/Jugador.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Entidades
+{
+    public abstract class Jugador : ICalculaPuntos
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public bool Expulsado { get; set; }
+        public Jugador(int id, string nombre, string apellido, bool expulsado)
+        {
+            Id = id;
+            Nombre = nombre;
+            Apellido = apellido;
+            Expulsado = expulsado;
+        }
+        public int AjusteExpulsado()
+        {
+            if (Expulsado)
+            {
+                return Constantes.PUNTAJE_EXPULSADO;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public abstract int CalcularPuntos();
+    }
+}
